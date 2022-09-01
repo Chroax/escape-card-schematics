19f5d385f1473810291a7466ea1bc139af5c0fd2
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-09-01 16:39:03 +0700

    add close button ui

---
+++ Assets/Scripts/UI/CloseButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseButton : MonoBehaviour
+{
+    [SerializeField] GameObject gameObject;
+    // Start is called before the first frame update
+    public void DeactivePanel()
+    {
+        if(gameObject.activeInHierarchy)
+            gameObject.SetActive(false);
+    }
+}
