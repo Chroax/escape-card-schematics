a482688e25411e4f96ca2930f5e408128a75ace5
SRYian <92420947+SRYian@users.noreply.github.com>
2022-09-02 00:43:55 +0700

    Add Script DBManager.cs, LoginScript.cs Add Script connection.php, login.php Add gameObj Logo Add gameObj Background Add gameObj LoginController Add gameObj UsernameTag Add gameObj PasswordTag Add gameObj UsernameField Add gameObj PasswordField Add gameObj LoginButton Add gameObj WarningMessage

---
+++ Assets/Scripts/Login Scripts/PHPScripts/login.php
@@ -0,0 +1,33 @@
+<?php
+
+    include('connection.php');
+
+    //  the next view variables are for the contents of the post method
+
+    // clear sql like syntax to prevent hackerman
+
+    $username=mysqli_real_escape_string($connect, $_POST["name"]);
+    $usernameClean=filter_var($username, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+    $password=$_POST["password"];
+       
+    $checkquery = "SELECT `username` , `password`, `score` FROM `users` WHERE `username`='".$usernameClean."';";
+    $checkname = mysqli_query($connect, $checkquery) or die("name check failed");
+    
+    if (mysqli_num_rows($checkname)!=1) {
+        
+        echo "2\tuser with no name lah";
+        exit;
+    }
+    
+    $info = mysqli_fetch_assoc($checkname);
+    $hashPass=md5($password);
+    $hashDB=$info["password"];
+    
+    if ($hashPass != $hashDB) {
+        echo "1\tFailure";
+        exit;
+    }
+   
+    echo "0\t". $info["score"];
+
+?>
