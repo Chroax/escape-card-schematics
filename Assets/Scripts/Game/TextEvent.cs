50a700e3ed0db15ed610ed833aea8de1536bc0a3
Daviann4 <71868354+davianoh@users.noreply.github.com>
2022-09-07 18:25:01 +0700

    cut scene mekanis
    udah ada base templatenya, tinggal bikin u/ smua cut scene

---
+++ Assets/Scripts/Game/TextEvent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TextEvent : MonoBehaviour
+{
+    private TMP_Text text;
+    [SerializeField] private List<string> dialoges;
+
+    private void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    public void ChangeTextEvent(int index)
+    {
+        text.text = dialoges[index];
+    }
+}
