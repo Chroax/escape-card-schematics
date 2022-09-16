030164dd7c1f019182f46a4bb58afd7db2b7f89c
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-09-16 07:56:48 +0700

    debug close button

--- Assets/Scripts/UI/CloseButton.cs
+++ Assets/Scripts/UI/CloseButton.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CloseButton : MonoBehaviour
 {
     [SerializeField] GameObject gameObject;
+    [SerializeField] GameObject audioManager;
 
     // Start is called before the first frame update
-    public void DeactivePanel()
+    public void DeactivePanel(bool isMainMenu)
     {
-        GameManager.Instance.audioManager.GetComponent<SoundManager>().clickSoundPlay();
+        if (isMainMenu)
+            audioManager.GetComponent<SoundManager>().clickSoundPlay();
+        else
+            GameManager.Instance.audioManager.GetComponent<SoundManager>().clickSoundPlay();
+
         if (gameObject.activeInHierarchy)
             this.gameObject.SetActive(false);
     }
