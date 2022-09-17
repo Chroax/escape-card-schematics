529ab8ec5e5dec5f4b0440d87f4dbc6dca003592
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-09-17 19:09:21 +0700

    add-scenechange-losewin

--- Assets/Scripts/UI/WinPanel.cs
+++ Assets/Scripts/UI/WinPanel.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinPanel : MonoBehaviour
 {
     private void OnEnable()
     {
         GameManager.Instance.audioManager.GetComponent<SoundManager>().winSoundPlay();
+    }
+
+    public void toMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 }
